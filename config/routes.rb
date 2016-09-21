f091c49e1ffe838576b5ed06c42d40584a8f3370
AngeLopez <angel@angelopez.ca>
2016-09-21 14:52:27 -0400

    User and log in created

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
-
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
